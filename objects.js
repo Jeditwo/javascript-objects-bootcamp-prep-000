0112c0d3f6ac67f4c581f2aac54c44c02ee1b15c
Joshua Wessels <joshuawessels04@gmail.com>
2019-04-22 14:55:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {
   Tobymac: "Eye on it",
   ColtonDixon: "More of you",
-  MathewWest: "Hello my name is"
+  MatthewWest: "Hello my name is"
 }
 
 function updatePlaylist(playlist, artist, song) {
